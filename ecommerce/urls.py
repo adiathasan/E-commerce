588511bd3f87bbe6cb901b7b24378960da89051c
adiathasan <adiathasan.me@gmail.com>
2020-04-30 04:50:30 -0700

    frontend done.

---
+++ ecommerce/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('register/', register_page, name='register'),
+    path('login/', login_page, name='login'),
+    path('shopping-cart/', shopping_cart, name='shopping-cart'),
+    path('shop/', shop, name='shop'),
+    path('contact/', contact_page, name='contact'),
+]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'ecommerce/index.html')
+
+
+def register_page(request):
+    return render(request, 'ecommerce/register.html')
+
+
+def login_page(request):
+    return render(request, 'ecommerce/login.html')
+
+
+def shopping_cart(request):
+    return render(request, 'ecommerce/shopping-cart.html')
+
+
+def shop(request):
+    return render(request, 'ecommerce/shop.html')
+
+
+def contact_page(request):
+    return render(request, 'ecommerce/contact.html')
