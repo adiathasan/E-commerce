431182f18fc691ebaa22e2e33d6f8b220fd93ad4
adiathasan <adiathasan.me@gmail.com>
2020-05-07 10:56:52 -0700

    final commit 2 ..

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kso$fr3i=mig0-4j6ir@ed5@ia1f+bs7utd63u^gd)qg$0y)m&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'e-commerce-1134.herokuapp.com']
 
