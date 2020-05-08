33e12cf0c75a210839f947fc459fd3562410efbd
Adiat Hasan <61478071+adiathasan@users.noreply.github.com>
2020-05-08 09:58:21 -0700

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -137,7 +137,7 @@ AWS_STORAGE_BUCKET_NAME = 'testing-e-commerce'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
-print(AWS_ACCESS_KEY_ID)
